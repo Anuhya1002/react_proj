463ea800839ea2a2a75d97f9b11edbfca91dcc5a
Anuhya1002 <anuhya27@gmail.com>
2022-12-28 23:01:00 +0530

    mapbox

--- src/Components/dashboard/dashboard.js
+++ src/Components/dashboard/dashboard.js
@@ -1,7 +1,100 @@
-import { Component } from "react";
+import mapboxgl from "mapbox-gl";
+import React, { Component } from "react";
+import "../../App.css";
+import "mapbox-gl/dist/mapbox-gl.css"
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoieWFzaW41NDk5MCIsImEiOiJjbGM3dnptNmYwM3JvM3BvMjVhYWxzZHB2In0.PAWijz529Uqet82VLTKPHA";
 
 export class Dashboard extends Component {
-    render() {
-        return (<h1>Dashboard</h1>);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      lng: -70.9,
+      lat: 42.35,
+      zoom: 10,
+    };
+    this.mapContainer = React.createRef();
+  }
+  componentDidMount() {
+    const { lng, lat, zoom } = this.state;
+    const map = new mapboxgl.Map({
+      container: this.mapContainer.current,
+      style: "mapbox://styles/yasin54990/clc7wy1g9001k14s08089ybkt",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+  }
+
+  render() {
+    return (
+      <div className="mainframe">
+        <div className="flex-row center-aligned space-between full-height">
+          <div className="flex-item sidemenu flex-column">
+            <h2 className="h2">Dashboard</h2>
+            <div>
+              <p className="sideMenuItem active">Overview</p>
+              <p className="sideMenuItem">Inbox</p>
+              <p className="sideMenuItem">Analytics</p>
+              <p className="sideMenuItem">Contacts</p>
+              <p className="sideMenuItem">Reports</p>
+              <p className="sideMenuItem">Profile</p>
+              <p className="sideMenuItem">Releases</p>
+            </div>
+
+            <button className="button primary">Logout</button>
+          </div>
+          <div className="flex-item maincontent full-height">
+            <div className="flex-column gap">
+              <div className="flex-row space-between navbar p-lr p-ub">
+                <h2>Good Morning, Williams</h2>
+                <div className="flex-row center-aligned gap">
+                  <button className="button primary outline">
+                    Upgrade now
+                  </button>
+                  <div className="avatar"></div>
+                </div>
+              </div>
+              <div className="flex-row space-between p-lr">
+                <div className="borderbox flex-column p-lr">
+                  <h4>users</h4>
+                  <h1 className="h1">20.3K</h1>
+                  <h4>
+                    <span className="label green">12%</span> vs last Month
+                  </h4>
+                </div>
+                <div className="borderbox flex-column p-lr">
+                  <h4>sessions</h4>
+                  <h1 className="h1">283.5K</h1>
+                  <h4>
+                    <span className="label red">32%</span> vs last Month
+                  </h4>
+                </div>
+                <div className="borderbox flex-column p-lr">
+                  <h4>spend times</h4>
+                  <h1 className="h1">34m 52s</h1>
+                  <h4>
+                    <span className="label green">52%</span> vs last Month
+                  </h4>
+                </div>
+                <div className="borderbox flex-column p-lr">
+                  <h4>Visits</h4>
+                  <h1 className="h1">152k</h1>
+                  <h4>
+                    <span className="label green">88%</span> vs last Month
+                  </h4>
+                </div>
+              </div>
+
+              <div className="p-lr p-ub">
+                <div>
+                  <div ref={this.mapContainer} className="map-container" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import {Login} from './Components/Login/login';
-import { Signup } from './Components/signup/signup';
-import './index.css';
-import { Dashboard } from './Components/dashboard/dashboard';
-
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Login } from "./Components/Login/login";
+import { Signup } from "./Components/signup/signup";
+import "./index.css";
+import { Dashboard } from "./Components/dashboard/dashboard";
 
 const isuser = false;
 const user = {
@@ -14,16 +13,14 @@ const user = {
   email: "sophie@gmail.com",
   password: "123456",
   isLoggedIn: true,
-  isVerified: true
-}
+  isVerified: true,
+};
 
 // localStorage.setItem("user", JSON.stringify(user));
 
-
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-root.render(<App />);
-
-
